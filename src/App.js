00e07eace4ba5baaf9768765ffc14b51e80fa463
SoniaCao <codewhitex@gmail.com>
2021-09-07 09:23:55 +1000

    HeaderFooter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-//import logo from "./logo.svg";
 import "./App.css";
-//import { Navbar, NavbarBrand } from "reactstrap";
 import React, { Component } from "react";
+import Main from "./components/MainComponent";
+
+//import { Navbar, NavbarBrand } from "reactstrap";
 //import Menu from "./components/MenuComponent";
 //import { DISHES } from "./shared/dishes";
-import Main from "./components/MainComponent";
 
 class App extends Component {
   render() {
