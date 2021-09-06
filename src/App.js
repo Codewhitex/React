909d8a02df3f6e3826f15c0e9ebd090f85b94fc9
SoniaCao <codewhitex@gmail.com>
2021-09-06 19:30:09 +1000

    commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+//import logo from "./logo.svg";
+import "./App.css";
+//import { Navbar, NavbarBrand } from "reactstrap";
+import React, { Component } from "react";
+//import Menu from "./components/MenuComponent";
+//import { DISHES } from "./shared/dishes";
+import Main from "./components/MainComponent";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
+  }
+}
+
+export default App;
