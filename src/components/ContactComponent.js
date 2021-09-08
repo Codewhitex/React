b18b2bb3796bb374f84c30235ee57aed338525b5
SoniaCao <codewhitex@gmail.com>
2021-09-08 12:26:24 +1000

    uncontrolledFormHeader

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -160,7 +160,7 @@ class Contact extends Component {
             </div>
           </div>
         </div>
-        {/* controlled form */}
+
         <div className="row row-content">
           <div className="col-12">
             <h3>Send us your Feedback</h3>
